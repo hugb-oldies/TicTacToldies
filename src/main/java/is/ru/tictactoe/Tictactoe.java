f80f7d8cef54e43786976b75ddd708f42dd0bb5e
Bjarki Kristinsson <bjarkik12@ru.is>
2015-10-21 23:29:46 +0000

    Branch test

--- src/main/java/is/ru/tictactoe/Tictactoe.java
+++ src/main/java/is/ru/tictactoe/Tictactoe.java
@@ -1,5 +1,25 @@
 package is.ru.stringcalculator;
+
 import java.util.*;
 import java.util.regex.Matcher;
+import java.fx.*;
+
 public class  Tictactoe{
+
+    public static void main(String[] args){
+	launch(args);
+    } 
+
+    public void start (Stage primaryStage) throws Exception{
+	primaryStage.setTitle("asdf");
+	button = new Button();
+	button.setText("Click me");
+
+	StackPane layout = new StackPane();
+	layout.getChildren().add(button);
+	
+	Scene scene = new Scene(layout, 300, 250);
+	primaryStage.setScene(scene);
+	primaryStage.show();
+    }
 }
