d256eb2ed602dbaec9bf7dd9e027f4ebc3e8da39
hraunfjord <magnusk12@ru.is>
2015-10-29 10:07:45 -0700

    fixing desplayBoard printout

--- src/main/java/is/ru/tictactoe/TictactoeConsole.java
+++ src/main/java/is/ru/tictactoe/TictactoeConsole.java
@@ -14,20 +14,27 @@ public class TictactoeConsole{
 
 		Board.initBoard();
 		displayBoard();		
+		// main loop
+		for (int i=0; i<9; i++) {
+
+		}
 
 	}
 
 
 	public static void displayBoard() {
+		System.out.println("-------");
 		for (int i=0; i<3; i++) {
+			System.out.print("|");
 			for (int j=0; j<3; j++) {
 				String toPrint = "1";
 				if (Board.checkSquare(i,j) == 0 ) toPrint = " ";
 				else if (Board.checkSquare(i,j) == 2 ) toPrint = "x";
-				System.out.print( toPrint + " " );
+				System.out.print( toPrint + "|" );
 			}
 			System.out.println("");
 		}
+		System.out.println("-------");
 	}
 
 }
